41cf53701c1466b2e04d233a4a5d6431ba52c4fb
rlarldud1234 <rlarldud6482@naver.com>
2022-07-25 12:44:13 +0900

    ⚡️ :: AuthButton Custom

--- Muleobolram/components/AuthButton.tsx
+++ Muleobolram/components/AuthButton.tsx
@@ -1,2 +1,36 @@
 import React from 'react';
 import {TouchableOpacity, Text, StyleSheet} from 'react-native';
+
+interface ButtonProp {
+  title: string;
+  buttonColor: string;
+  tintColor: string;
+  onPress: () => void;
+}
+
+const AuthButton = (props: ButtonProp) => {
+  return (
+    <TouchableOpacity
+      style={[styles.button, {backgroundColor: props.buttonColor}]}
+      onPress={props.onPress}>
+      <Text style={[styles.title, {color: props.tintColor}]}>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    height: 60,
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+  },
+});
+
+export default AuthButton;
