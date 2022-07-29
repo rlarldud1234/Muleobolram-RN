f65f8a3fbd62d471fb262647b8f82214e0c872c4
rlarldud1234 <rlarldud6482@naver.com>
2022-07-30 00:04:08 +0900

    ⚡️ :: 파일 위치 변경하기

---
+++ Muleobolram/screens/auth/Login.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {Text, StyleSheet, View} from 'react-native';
+import {SafeAreaView} from 'react-navigation';
+import {AuthButton} from '../../components/auth';
+import {AuthTextField} from '../../components/auth';
+
+const LoginScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text style={styles.text1}>물어</Text>
+      <Text style={styles.text2}>볼램</Text>
+      <View style={styles.textInput}>
+        <AuthTextField placeholder={'아이디를 입력하세요.'} />
+        <AuthTextField placeholder={'비밀번호를 입력하세요.'} />
+      </View>
+      <View style={styles.button}>
+        <AuthButton
+          buttonColor="#008000"
+          title="로그인"
+          tintColor="#ffffff"
+          onPress={() => console.log('login')}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  text1: {
+    fontSize: 50,
+    color: 'black',
+    alignContent: 'center',
+    justifyContent: 'center',
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginLeft: 16,
+  },
+  text2: {
+    fontSize: 50,
+    color: 'black',
+    alignContent: 'center',
+    justifyContent: 'center',
+    fontWeight: 'bold',
+    marginLeft: 16,
+  },
+  textInput: {
+    top: 50,
+    marginHorizontal: 16,
+  },
+  button: {
+    top: 100,
+    marginHorizontal: 16,
+  },
+});
+
+export default LoginScreen;
---
+++ Muleobolram/screens/auth/SignUp.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {SafeAreaView} from 'react-navigation';
+
+const SignupScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>회원가입</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SignupScreen;
