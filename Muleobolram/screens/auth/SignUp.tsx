94bb409570ed9827f72b09c0e3d666091cf51314
rlarldud1234 <rlarldud6482@naver.com>
2022-07-31 01:43:10 +0900

    ⚡️ :: View Publishing 외에 navigators 설정

--- Muleobolram/screens/auth/SignUp.tsx
+++ Muleobolram/screens/auth/SignUp.tsx
@@ -1,13 +1,49 @@
+import {StackNavigationProp} from '@react-navigation/stack';
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {SafeAreaView} from 'react-navigation';
+import {AuthButton, AuthTextField} from '../../components/auth';
+import {RootScreens, RootStackList} from '../../navigators';
 
-const SignupScreen = () => {
+type SignUpScreenNavigationProps = StackNavigationProp<
+  RootStackList,
+  RootScreens.Signup
+>;
+
+interface SignUpScreenProps {
+  navigation: SignUpScreenNavigationProps;
+}
+
+const SignupScreen: React.FunctionComponent<SignUpScreenProps> = props => {
+  const {navigation} = props;
   return (
     <SafeAreaView>
-      <Text>회원가입</Text>
+      <View style={styles.textField}>
+        <AuthTextField placeholder="이름을 입력하세요." />
+        <AuthTextField placeholder="아이디를 입력하세요." />
+        <AuthTextField placeholder="비밀번호를 입력하세요." />
+      </View>
+      <View style={styles.button}>
+        <AuthButton
+          buttonColor="#008000"
+          tintColor="#ffffff"
+          title="회원가입"
+          onPress={() => navigation.pop()}
+        />
+      </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  textField: {
+    top: 50,
+    marginHorizontal: 20,
+  },
+  button: {
+    top: 100,
+    marginHorizontal: 20,
+  },
+});
 
 export default SignupScreen;
